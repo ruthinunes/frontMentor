b8ac0ce07f5fc2847cfad39921ad3eec6cd5afb0
ruthinunes <ruthinunesdev@gmail.com>
2023-01-04 18:56:42 -0300

    setting AJAX for validate email

--- newbee/Comming soon page/index.js
+++ newbee/Comming soon page/index.js
@@ -1,15 +1,28 @@
 const emailInput = document.querySelector('#emainInput');
+const allInputs = document.querySelectorAll('.form_email');
 const smallError = document.querySelector('#smallError');
+const formBtn = document.querySelector('#form');
 
-document.querySelector('#form').addEventListener('submit', function (e) {
+formBtn.addEventListener('submit', function (e) {
     e.preventDefault();
 
     if (emailInput.value == "") {
         smallError.classList.add('error');
         emailInput.classList.add('error');
+        smallError.innerHTML = 'Whoops! It looks like you forgot to add your email'
+
+    } else if (!emailValidation(emailInput.value)) {
+        smallError.classList.add('error');
+        emailInput.classList.add('error');
+        smallError.innerHTML = 'Please provide a valid email address'
     } else {
         smallError.classList.remove('error');
         emailInput.classList.remove('error');
         alert('Verified email');
     }
 });
+
+function emailValidation(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
